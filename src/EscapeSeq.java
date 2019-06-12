f3751fdbb553bbeb1e304d71825743ad83acf828
xiongbob <xiong.bob@gmail.com>
2019-06-12 17:04:07 -0400

    whatever-commit-EscapeSeq

---
+++ src/EscapeSeq.java
@@ -0,0 +1,13 @@
+
+
+public class EscapeSeq {
+    public static void main (String [] args) {
+        System.out.println ("Print the following columns of values. Use \\t for the tabs between the columns.");
+
+        System.out.print ("Cost\t      Quantity\t      Total\t\n");
+        System.out.print ("$1,000.00\t   4\t       $4,000.00\t\n");
+        System.out.print ("$   50.00\t   8\t         $400.00\t\n");
+        System.out.print ("-----------------------------------\n");
+        System.out.print ("TOTAL:\t      12\t      $4,400.00\t\n");
+    }
+}
